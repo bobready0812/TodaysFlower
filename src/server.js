d3692ec538fcdd13fce3cff4f767abd5a761ffbd
DaehyeonKim <Daehyeonoff@gmail.com>
2022-03-23 17:13:03 +0900

    20220323

--- src/server.js
+++ src/server.js
@@ -5,18 +5,17 @@ const PORT = 5000;
 
 const app = express();
 const logger = morgan("dev");
-
-const home = (req, res) => {
-    return res.send("hello");
-}
-
-const login = (req, res) => {
-    return res.send("login");
-}
- 
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+
+const globalRouter = express.Router();
+const userRouter = express.Router();
+const videoRouter = express.Router();
+
+app.use("/",globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+ 
+
 
 const handleListening = () => console.log(`Server listening on port ${PORT}`);
 
