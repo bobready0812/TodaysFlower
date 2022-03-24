682311fff87d5c83f0dd72891a14c3ecfdc25111
DaehyeonKim <Daehyeonoff@gmail.com>
2022-03-24 17:00:16 +0900

    20220324

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -6,7 +6,7 @@ const videoRouter = express.Router();
 const handleWatchVideo = (req,res) => res.send("Watch Video")
 
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 export default videoRouter;
