5b4ad5b9438d2fc0ec47b1d134bdf48b04f06786
DaehyeonKim <Daehyeonoff@gmail.com>
2022-03-25 17:58:41 +0900

    20220325

--- src/routers/onRouter.js
+++ src/routers/onRouter.js
@@ -1,7 +1,11 @@
 import express from "express";
+import { profile, setting, controlPot, write } from "../controllers/onController";
 
 const onRouter = express.Router();
 
-onRouter.get("/", link);
+onRouter.get("/:user",profile);
+onRouter.get("/:FB_id ",profile);
+onRouter.get("/:user/FB_id/setting",profile);
+onRouter.get("/:user/FB_id/write", write)
 
 export default onRouter;
