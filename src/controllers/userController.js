c4546d4e5b3c2e43945946cacc3f5f677c2bb776
DaehyeonKim <Daehyeonoff@gmail.com>
2022-03-29 19:45:15 +0900

    20220329

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,11 +2,15 @@ export const join = (req,res) => {
     res.render("join");
 }
 
+export const getJoin = (req,res) => {
+    res.render("join");
+}
+
 export const postJoin = (req,res) => {
     console.log(req.body);
     res.redirect("/");
 }
 
-export const login = (req,res) => {
+export const getLogin = (req,res) => {
     res.send("login");
 }
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,14 +1,13 @@
 import express from "express";
 import { main,choose } from "../controllers/globalController"; 
-import { join, postJoin, login } from "../controllers/userController";
+import { getJoin, postJoin, getLogin } from "../controllers/userController";
 
 const globalRouter = express.Router();
 
 globalRouter.get("/", main);
 globalRouter.get("/switch",choose);
-globalRouter.get("/join", join);
-globalRouter.post("/join", postJoin);
-globalRouter.get("/login",login);
+globalRouter.route("/join").get(getJoin).post(postJoin);
+globalRouter.get("/login",getLogin);
 //로딩 라우터는 나중에 추가
 
 export default globalRouter;
