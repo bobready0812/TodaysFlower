34cddeeeaaf7b4ef6f01aabd44c59c5db2b2586c
DaehyeonKim <Daehyeonoff@gmail.com>
2022-03-30 17:03:33 +0900

    20220330

--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,15 @@
+import bcrypt from "bcrypt"
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   id: {type: String, required: true, unique: true},
   password: { type :String, required: true},
   username: {type: String, required: true, unique: true},
+});
+
+
+userSchema.pre('save', async function(){
+  this.password = await bcrypt.hash()
 })
 
 const User = mongoose.model('User', userSchema);
