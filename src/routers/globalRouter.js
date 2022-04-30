a937ddb5237d5b4cc152741ee9aef62ee957b501
DaehyeonKim <Daehyeonoff@gmail.com>
2022-04-30 19:47:20 +0900

    20220430

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -8,6 +8,7 @@ globalRouter.get("/", main);
 globalRouter.get("/switch",choose);
 globalRouter.route("/join").get(getJoin).post(postJoin); 
 globalRouter.route("/login").get(getLogin).post(postLogin);
+globalRouter.get("/practice", prac);
 //로딩 라우터는 나중에 추가
 
 export default globalRouter;
